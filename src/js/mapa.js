9d2b3813679f5cb9eb33ee02e495edf89da706e2
daniela <moreno.tobar.daniela@gmail.com>
2022-09-11 20:13:18 -0500

    city config on the map

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -5,6 +5,9 @@
     const lng = -76.5266211;
     const mapa = L.map('mapa').setView([lat, lng ], 13);
     let marker;
+
+    //Utilizar Provider y Geocoder
+    const geocodeService = L.esri.Geocoding.geocodeService();
     
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -17,6 +20,22 @@
         autoPan: true
     })
     .addTo(mapa);
+
+    //Detectar movimiento del pin
+    marker.on('moveend', function(e){
+        
+        marker = e.target
+        const posicion = marker.getLatLng();
+        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng)); //Donde se suelte el pin ahi se centrará el mapa
+
+        //Obtener la direccion e info de las calles al soltar el pin 
+        geocodeService.reverse().latlng(posicion, 13).run(function(error, resultado){
+            //console.log(resultado);
+
+            //Globo encima del pin
+            marker.bindPopup(resultado.address.LongLabel);
+        })
+    })
 
 
 })()
