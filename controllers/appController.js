3e34897fa0bd143bf870371319a5b07ca54b54c7
daniela <moreno.tobar.daniela@gmail.com>
2022-09-22 19:24:21 -0500

    filters

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,8 +1,17 @@
+import { Precio, Categoria, Propiedad } from '../models/index.js';
 
-const inicio = (req, res) => {
+
+const inicio = async (req, res) => {
+
+    const [ categorias, precios ] = await Promise.all([
+        Categoria.findAll({raw: true}),
+        Precio.findAll({raw: true}),
+    ])
 
     res.render('Inicio', {
-        pagina: 'Inicio'
+        pagina: 'Inicio',
+        categorias,
+        precios
     });
 }
 
--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -4,6 +4,7 @@
     const lng = -76.5266211;
     const mapa = L.map('mapa-inicio').setView([lat, lng ], 13);
 
+    let markers = new L.FeatureGroup().addTo(mapa);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,11 +14,42 @@
         try{
 
             const url = '/api/propiedades';
-            
+            const respuesta = await fetch(url); //Ver si la conexion es correcta
+            const propiedades = await respuesta.json(); //Retornar la respuesta en formato json
+
+            mostrarPropiedades(propiedades); 
+
         }catch (error){
             console.log(error)
         }
     }
+
+
+    const mostrarPropiedades = propiedades => {
+        
+        propiedades.forEach(propiedad => {
+
+            //Agregar los pines
+            const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
+                autoPan: true
+            })
+            .addTo(mapa)
+            .bindPopup(`
+                <p class="text-greendark text-base font-bold">${propiedad.categoria.nombre}</p>
+                <h1 class="text-lg text-greendark font-extrabold pb-2">${propiedad?.titulo}</h1>
+                <img src="/uploads/${propiedad?.imagen}" alt="Imagen de la propiedad ${propiedad.titulo}">
+                <p class="text-blue font-bold">${propiedad.precio.nombre}</p>
+                <a href="/propiedad/${propiedad.id}" class="bg-greendark block p-2 text-center font-bold">Ver Propiedad</a>
+            `)
+
+
+            markers.addLayer(marker);
+        })
+
+    }
+
+
+
 
     obtenerPropiedades();
 
